7f38d14988ba12a4f1131ed0ad2e490782f36892
Ben <52554863+hubertben@users.noreply.github.com>
2020-04-25 13:29:42 -0400

    Add files via upload
    Basic Functions without Genetic Algorithm

---
+++ TP/card.py
@@ -0,0 +1,18 @@
+
+class Card:
+
+    def __init__(self, ID, value, suite, sv, ss, abs_val):
+        self.ID = ID
+        self.suite = suite
+        self.value = value
+        self.ss = ss
+        self.sv = sv
+        self.abs_val = abs_val
+
+        
+        
+    def print_long(self):
+        print(self.value, " of ", self.suite, end = ' ')
+
+    def print_short(self):
+        print(self.ss + "" + self.sv, end = ' ')
